75c9f64d8a9eecc6aaf270426fd55d7667f1c498
Wyatt Wilgus <wwilgus@email.sc.edu>
2021-10-16 23:12:24 -0400

    Commit 0: UI Class and Method Stubs

---
+++ InternshipUI.java
@@ -0,0 +1,399 @@
+import java.util.Scanner;
+
+/**
+ * Contains all user interfaces.
+ * @author Wyatt Wilgus
+ */
+public class InternshipUI {
+    private Scanner read = new Scanner(System.in);
+//    private InternshipApplication application;
+
+    public void run() {
+
+    }
+
+    /**
+     * Entry UI
+     * @options (L)ogin, (C)reate Account, (E)xit
+     */
+    public void StartUI() {
+
+    }
+
+    /**
+     * Login UI
+     */
+    public void Login() {
+
+    }
+
+    /**
+     * Askes user which type of account they want to create
+     * @options (S)student, (E)mployer, (B)ack
+     */
+    public void CreateAccount() {
+
+    }
+
+    /**
+     * Creates a new student account
+     */
+    public void CreateStudent() {
+
+    }
+
+    /**
+     * Creates a new employer account
+     */
+    public void CreateEmployer() {
+
+    }
+
+    /**
+     * Main menu for students after login
+     * @options (C)reate Resume, (O)ptions, (R)ate Employer, (V)iew Internships, (L)ogout
+     */
+    public void StudentMain() {
+
+    }
+
+    /**
+     * Meain menu for employers after login
+     * @options (C)reate Internship, (V)iew Internship, (E)mployees, (B)ack, (L)ogout
+     */
+    public void EmployerMain() {
+
+    }
+
+    /**
+     * Main menu for admins after login
+     * @options 
+     */
+    public void AdminMain() {
+
+    }
+
+    /**
+     * UI for creating a new resume
+     * @options (S)hool, (W)ork Experience, (E)xtra-Curricular, (D)one, (B)ack, (L)ogout
+     */
+    public void CreateResume() {
+
+    }
+
+    /**
+     * UI for entering ecucation
+     */
+    public void EnterEducation() {
+
+    }
+
+    /**
+     * UI for entering work experiance
+     */
+    public void EnterWork() {
+
+    }
+
+    /**
+     * UI for entering extra-curricular
+     */
+    public void EnterExtra() {
+
+    }
+
+    /**
+     * Menu for student account settings
+     * @options (P)rofile Options, (R)esume Options, (B)ack, (L)ogout
+     */
+    public void AccountOptions() {
+
+    }
+
+    /**
+     * Profile options menu
+     * @options (E)mail, (P)assword, (C)urrent Employer, (B)ack, (L)ogout
+     */
+    public void ProfileOptions() {
+
+    }
+
+    /**
+     * UI for changing email
+     */
+    public void ChangeEmail() {
+
+    }
+
+    /**
+     * UI for changing password
+     */
+    public void ChangePassword() {
+
+    }
+
+    /**
+     * UI for changing current employer
+     */
+    public void ChangeEmployer() {
+
+    }
+
+    /**
+     * Menu for viewing and editing a resume
+     * @options (S)kills, (W)ork Experience, (S)chool, (E)xtra-Curriculars, (C)urrent Employer, (B)ack, (L)ogout
+     */
+    public void ResumeOptions() {
+
+    }
+
+    /**
+     * UI for editing a list of skills
+     */
+    public void EditSkills() {
+
+    }
+
+    /**
+     * UI for adding work Experience
+     */
+    public void AddWork() {
+
+    }
+
+    /**
+     * UI for editing education
+     */
+    public void EditEducation() {
+
+    }
+
+    /**
+     * UI for editing extra-curriculars
+     */
+    public void EditExtra() {
+
+    }
+
+    /**
+     * UI for editing current employer
+     */
+    public void EditCurrent() {
+
+    }
+
+    /**
+     * UI for adding a rating
+     */
+    public void AddRating(/*Employer*/) {
+
+    }
+
+    /**
+     * Menu for viewing internship lists
+     * @options (F)ull List, (S)earch, (B)ack, (L)ogout
+     */
+    public void ViewInternshipList() {
+
+    }
+
+    /**
+     * Displays and internships information
+     * @options (A)pply, (B)ack, (L)ogout
+     */
+    public void ViewInternship() {
+
+    }
+
+    /**
+     * Menu for searching internships
+     * @options (C)ompany, (P)osition, (S)alary, (B)ack, (L)ogout
+     */
+    public void SeachInternships() {
+
+    }
+
+    /**
+     * Menu for searching internships by company
+     */
+    public void SearchCompany() {
+
+    }
+
+    /**
+     * UI for searching internships by position
+     */
+    public void SearchPosition() {
+
+    }
+
+    /**
+     * Displays a list of internships
+     * @options (#)of Listing, (N)ext, (P)revious, (B)ack, (L)ogout
+     */
+    public void DisplayResults() {
+
+    }
+
+    /**
+     * Displays information about an internship
+     * @options (A)pply, (B)ack, (L)ogout
+     */
+    public void InternshipInfo() {
+
+    }
+
+    /**
+     * UI for creating an internship
+     */
+    public void CreateInternship() {
+
+    }
+
+    /**
+     * UI for adding salary to an internship
+     */
+    public void AddSalary() {
+
+    }
+
+    /**
+     * UI for adding a schedule to an internship
+     */
+    public void AddSchedule() {
+
+    }
+
+    /**
+     * Displays a list of employees
+     * @options (#)of Listing, (N)ext, (P)revious, (B)ack, (L)ogout
+     */
+    public void ViewEmployees() {
+
+    }
+
+    /**
+     * UI for adding a rating to a employee
+     */
+    public void AddRating(/*Student*/) {
+
+    }
+
+    /**
+     * UI for removing an employee
+     */
+    public void RemoveEmployee() {
+
+    }
+
+    /**
+     * Displays a list of internships
+     * @options (#)of Listing, (N)ext, (P)revious, (B)ack, (L)ogout
+     */
+    public void ViewInternships() {
+
+    }
+
+    /**
+     * Displays information about an internship
+     * @options (E)dit, (R)emove, (V)iew Applicants, (B)ack, (L)ogout
+     */
+    public void ViewInternship() {
+
+    }
+
+    /**
+     * Menu for editing an internship
+     * @options (C)ompany, (T)itle, (S)kills, (P)ay, (W)ork Schedule, (E)xperation Date, (B)ack, (L)ogout
+     */
+    public void EditInternship() {
+
+    }
+
+    /**
+     * Displays list of applicants
+     * @options (#)of Listing, (N)ext, (P)revious, (B)ack, (L)ogout
+     */
+    public void ViewApplications() {
+
+    }
+
+    /**
+     * Displays an appicants resume
+     * @options (A)ccept Applicant, (D)ecline Applicant, (B)ack, (L)ogout
+     */
+    public void ViewResume() {
+
+    }
+
+    /**
+     * Displays list of all student users
+     * @options (#)of Listing, (N)ext, (P)revious, (B)ack, (L)ogout
+     */
+    public void ViewStudents() {
+
+    }
+
+    /**
+     * Displays a student profile
+     * @options (P)romote, (R)emove, (V)iew Ratings, (B)ack, (L)ogout
+     */
+    public void ViewStudent() {
+
+    }
+
+    /**
+     * Displays a list of a students ratings
+     * @options (#)to Remove, (R)eset Rating, (N)ext, (P)revious, (B)ack, (L)ogout
+     */
+    public void ViewRatings() {
+
+    }
+
+    /**
+     * Displays a student's resume
+     * @options (R)emove, (B)ack, (L)ogout
+     */
+    public void ViewResume() {
+
+    }
+
+    /**
+     * Displays a list of employers
+     */
+    public void ViewEmployers() {
+
+    }
+
+    /**
+     * Displays a list of internships
+     * @options (#)of Listing, (N)ext, (P)revious, (B)ack, (L)ogout
+     */
+    public void ViewInternships() {
+
+    }
+
+    /**
+     * Views a list of employees
+     * @options (#)of Listing, (N)ext, (P)revious, (B)ack, (L)ogout
+     */
+    public void ViewEmployees() {
+
+    }
+
+    /**
+     * Displays an employee profile
+     * @options (A)dd Rating, (R)emove Employee, (B)ack, (L)ogout
+     */
+    public void ViewEmployee() {
+
+    }
+
+    /**
+     * Displays a users ratings
+     * @options (#)to Remove, (R)eset Rating, (N)ext, (P)revious, (B)ack, (L)ogout
+     */
+    public void ViewRatings() {
+
+    }
+}
