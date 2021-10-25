1b19836e8627bd6888cc6d9a3e2ce2aa435ca3a9
robbie6400 <79953519+robbie6400@users.noreply.github.com>
2021-10-24 22:07:24 -0400

    Update and rename workExpierence.java to WorkExpierence.java
    updated Work Experience class

---
+++ Model/WorkExpierence.java
@@ -0,0 +1,50 @@
+package Model;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class WorkExperience {
+
+	private String company;
+	private int workLength;
+	private Date start;
+	private Date end;
+	
+	public WorkExperience(String company, int workLength, String start, String end) throws ParseException {
+		
+		this.company = company;
+		this.workLength = workLength;
+		this.start = new SimpleDateFormat("dd/MM/yyyy").parse(start);
+		this.end = new SimpleDateFormat("dd/MM/yyyy").parse(end);
+	}
+
+	public int getWorkLength() {
+		return workLength;
+	}
+
+	public void setWorkLength(int workLength) {
+		this.workLength = workLength;
+	}
+
+	public Date getStart() {
+		return start;
+	}
+
+	public void setStart(Date start) {
+		this.start = start;
+	}
+
+	public Date getEnd() {
+		return end;
+	}
+
+	public void setEnd(Date end) {
+		this.end = end;
+	}
+	
+	public String toString() {
+		
+		return "Work Experience: /n" + this.company + ": " + this.start + "-" + this.end;
+	}
+}
