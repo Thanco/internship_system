7c0cea57c0af373ddfb8a679af6c8d63a2725a21
robbie6400 <79953519+robbie6400@users.noreply.github.com>
2021-10-28 02:43:44 -0400

    Update Resume.java
    updated Resume class

--- Model/Resume.java
+++ Model/Resume.java
@@ -1,27 +1,50 @@
 package Model;
 
-import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.UUID;
 
 public class Resume {
 
+	private UUID uuid;
+	private UUID uuidRes;
 	private String firstName;
 	private String lastName;
 	private Education education;
-	private WorkExperience workExpierence;
-	private ArrayList<String> studentSkills;
-	private ExtraCirricular extraCirricular;
+	private ArrayList<String> studentSkills = new ArrayList<String>();
+	private ArrayList<WorkExperience> workExperienceList = new ArrayList<WorkExperience>();
+	private ArrayList<ExtraCirricular> extraCirricularList = new ArrayList<ExtraCirricular>();
 	
-	public Resume(String firstName, String lastName, String school, SchoolYear schoolClass, String major, String company, 
-			int length, String start, String end, ArrayList<String> studentSkills, String extraCirricular) {
+	public Resume(UUID uuid, UUID uuidRes, String firstName, String lastName, Education education, ArrayList<String> studentSkills, 
+			ArrayList<WorkExperience> workExperienceList, ArrayList<ExtraCirricular> extraCirricularList ) {
 		
+		this.uuid = uuid;
 		this.firstName = firstName;
 		this.lastName = lastName;
-		this.education = new Education(school, schoolClass, major);
-		this.workExpierence = new WorkExperience(company, length, start, end);
+		this.education = education;
 		this.studentSkills = studentSkills;
-		this.extraCirricular = new ExtraCirricular(extraCirricular);
-		//this.currentWork = new CurrentWork();
+		this.workExperienceList = workExperienceList;
+		this.extraCirricularList = extraCirricularList;	
+	}
+	
+	public Resume(UUID uuid, String firstName, String lastName, Education education, ArrayList<String> studentSkills, 
+			ArrayList<WorkExperience> workExperienceList, ArrayList<ExtraCirricular> extraCirricularList ) {
+		
+		this.uuid = uuid;
+		this.uuidRes = UUID.randomUUID();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.education = education;
+		this.studentSkills = studentSkills;
+		this.workExperienceList = workExperienceList;
+		this.extraCirricularList = extraCirricularList;	
+	}
+	
+	public UUID getUuid() {
+		return uuid;
+	}
+
+	public UUID getUuidRes() {
+		return uuidRes;
 	}
 
 	public String getFirstName() {
@@ -48,14 +71,6 @@ public class Resume {
 		this.education = education;
 	}
 
-	public WorkExperience getWorkExpierence() {
-		return workExpierence;
-	}
-
-	public void setWorkExpierence(WorkExperience workExpierence) {
-		this.workExpierence = workExpierence;
-	}
-
 	public ArrayList<String> getStudentSkills() {
 		return studentSkills;
 	}
@@ -64,12 +79,55 @@ public class Resume {
 		this.studentSkills = studentSkills;
 	}
 
-	public ExtraCirricular getExtraCirricular() {
-		return extraCirricular;
+	public ArrayList<WorkExperience> getWorkExperienceList() {
+		return workExperienceList;
 	}
 
-	public void setExtraCirricular(ExtraCirricular extraCirricular) {
-		this.extraCirricular = extraCirricular;
+	public void setWorkExperienceList(ArrayList<WorkExperience> workExperienceList) {
+		this.workExperienceList = workExperienceList;
+	}
+
+	public ArrayList<ExtraCirricular> getExtraCirricularList() {
+		return extraCirricularList;
+	}
+
+	public void setExtraCirricularList(ArrayList<ExtraCirricular> extraCirricularList) {
+		this.extraCirricularList = extraCirricularList;
+	}
+
+	public void addWorkExperience(WorkExperience workExperience) {
+		
+		if (workExperienceList.size() >= 3) {
+			
+			workExperienceList.remove(2);
+			workExperienceList.add(workExperience);
+			sortWorkExperienceList();
+		}
+		
+		else {
+			
+			workExperienceList.add(workExperience);
+			sortWorkExperienceList();
+		}
+		
 	}
 	
+	public void sortWorkExperienceList() {
+		
+		for(int i = 0; i < workExperienceList.size()-1; i++) {
+			
+			if(workExperienceList.get(i).getStart().compareTo(workExperienceList.get(i+1).getStart()) > 0) {
+				
+				WorkExperience temp = workExperienceList.get(i);
+				workExperienceList.set(i, workExperienceList.get(i+1));
+				workExperienceList.set(i+1, temp);
+			}
+		}
+	}
+	
+	public void addExtraCirricular(ExtraCirricular extraCirricular) {
+		
+		extraCirricularList.add(extraCirricular);
+	}
+
 }
